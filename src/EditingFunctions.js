45ee32ff1437703329dd0827ecc2680ad8bfef6c
CodyAdam <cody.adam@laposte.net>
2020-02-08 21:12:08 +0100

    fix the modulo problem

--- src/EditingFunctions.js
+++ src/EditingFunctions.js
@@ -15,9 +15,14 @@ export function createMap(width, height) {
 // dans la classe Layer
 export function getTileCoordinateAtMousePos(mousePos, grid) {
     const offset = grid.offset;
-    const x = (mousePos.x - offset.x - ((mousePos.x - offset.x) % grid.size)) / grid.size;
-    const y = (mousePos.y - offset.y - ((mousePos.y - offset.y) % grid.size)) / grid.size;
+    const x = (mousePos.x - offset.x - mod(mousePos.x - offset.x, grid.size)) / grid.size;
+    const y = (mousePos.y - offset.y - mod(mousePos.y - offset.y, grid.size)) / grid.size;
+
     return { x: x, y: y };
+}
+
+function mod(n, m) {
+    return ((n % m) + m) % m;
 }
 
 //jsp
