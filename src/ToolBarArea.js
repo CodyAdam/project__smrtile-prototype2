15e49c1ddbc011f99cf8f261788aad3eb0207486
CodyAdam <cody.adam@laposte.net>
2020-02-13 17:16:59 +0100

    Add state to the ToolBar button and Update the css

--- src/ToolBarArea.js
+++ src/ToolBarArea.js
@@ -1,4 +1,5 @@
 import React from "react";
+import testImg from "./assets/tileset/test.png";
 
 class ToolBarArea extends React.Component {
 	constructor(props) {
@@ -8,12 +9,9 @@ class ToolBarArea extends React.Component {
 	render() {
 		return (
 			<div id="ToolBarArea">
-				<TileButton />
-				<TileButton />
-				<TileButton />
-				<TileButton />
-				<TileButton />
-				<TileButton />
+				<TileButton tileset={testImg} x={0} y={0} width={236} height={236} />
+				<TileButton tileset={testImg} x={0} y={0} width={236} height={236} />
+				<TileButton tileset={testImg} x={0} y={0} width={236} height={236} />
 				<TileButton />
 				<TileButton />
 			</div>
@@ -24,12 +22,27 @@ class ToolBarArea extends React.Component {
 class TileButton extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			tileset: props.tileset,
+			position: { x: props.x, y: props.y },
+			width: props.width,
+			height: props.height
+		};
 	}
+
+	componentDidMount() {
+		const ctx = this.refs.canvas.getContext("2d");
+		let img = new Image();
+		img.src = this.state.tileset;
+		ctx.fillStyle = "#FF0000";
+		ctx.fillRect(0, 0, 50, 50);
+		ctx.clearRect(2, 2, 46, 46);
+	}
+
 	render() {
 		return (
-			<div id="TileButton">
-				<button>IMG</button>
+			<div className="tileButton">
+				<canvas className="buttonCanvas" ref="canvas" width="50" height="50"></canvas>
 			</div>
 		);
 	}
