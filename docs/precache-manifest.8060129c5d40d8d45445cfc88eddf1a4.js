5d9e365ae6e13c834952229c59bb3f3c4961a4ef
CodyAdam <cody.adam@laposte.net>
2020-02-24 14:27:50 +0100

    Added docs folder (build)

---
+++ docs/precache-manifest.8060129c5d40d8d45445cfc88eddf1a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "009147882b59a265287412f058bb00b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "237c90fe462e91181a81",
+    "url": "./static/css/main.a9cd1ac5.chunk.css"
+  },
+  {
+    "revision": "6c9ebcce8bfbbba24941",
+    "url": "./static/js/2.b4323db7.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "./static/js/2.b4323db7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "237c90fe462e91181a81",
+    "url": "./static/js/main.36ec9f93.chunk.js"
+  },
+  {
+    "revision": "5398015fe133f1c7521c",
+    "url": "./static/js/runtime-main.5c8d6ba0.js"
+  },
+  {
+    "revision": "fcc611d3f6aa935279f0347dae8f24c7",
+    "url": "./static/media/cross.fcc611d3.svg"
+  },
+  {
+    "revision": "d315007b10c165ce1d1ae629357877fe",
+    "url": "./static/media/eye_close.d315007b.png"
+  },
+  {
+    "revision": "21a234f5a94877f8edd967a979ed0528",
+    "url": "./static/media/eye_open.21a234f5.png"
+  }
+]);
