10b0b55b25f49a145ce656d1f678c03e3dd07445
CodyAdam <cody.adam@laposte.net>
2020-02-26 14:13:50 +0100

    Build + Make unavailiable menu option grey

--- docs/precache-manifest.2cf1c3baebc1a7517a838aa5d7e4fab8.js
+++ docs/precache-manifest.2cf1c3baebc1a7517a838aa5d7e4fab8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3ababc6eefc2f662da42e15f19da85fc",
+    "revision": "f49d0c100cd36cbb92c8566aaaf020d1",
     "url": "./index.html"
   },
   {
-    "revision": "020ef3d17a7a2bfe773b",
-    "url": "./static/css/main.968b8dbc.chunk.css"
+    "revision": "c190c18d70c09d3c00c1",
+    "url": "./static/css/main.ac3056c0.chunk.css"
   },
   {
-    "revision": "44bbda4d67fb2ad32deb",
-    "url": "./static/js/2.53d50506.chunk.js"
+    "revision": "859e74f2552210d8c622",
+    "url": "./static/js/2.6995f97d.chunk.js"
   },
   {
-    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
-    "url": "./static/js/2.53d50506.chunk.js.LICENSE.txt"
+    "revision": "c60a93b7e8627dc76fb931ee62493de0",
+    "url": "./static/js/2.6995f97d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "020ef3d17a7a2bfe773b",
-    "url": "./static/js/main.0aa4296c.chunk.js"
+    "revision": "c190c18d70c09d3c00c1",
+    "url": "./static/js/main.ad51ddf9.chunk.js"
   },
   {
     "revision": "0eef35b3befc8704f6cc",
