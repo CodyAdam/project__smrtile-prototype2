6f4237027c8c8e2c3ce96618e7a86dbffd25b1b3
CodyAdam <cody.adam@laposte.net>
2020-04-01 20:13:27 +0200

    Update PropertiesPanel.js

--- src/Panel/PropertiesPanel.js
+++ src/Panel/PropertiesPanel.js
@@ -1,55 +1,55 @@
 import React from "react";
 
 export default class PropertiesPanel extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			panelName: "Information",
-			title: "How to use"
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            panelName: "Information",
+            title: "How to use",
+        };
+    }
 
-	changePanelSize(newSize) {
-		this.setState({ size: newSize });
-	}
+    changePanelSize(newSize) {
+        this.setState({ size: newSize });
+    }
 
-	render() {
-		let propertie = <div>this.props.selected;</div>;
-		if (this.props.selected !== null && this.props.selected !== undefined) {
-			propertie = (
-				<div>
-					<span className="subTitle">{this.props.selected.state.name} Properties</span>
-					<br />
-					<br />
-					{this.props.selected.state.properties.name}
-				</div>
-			);
-		}
+    render() {
+        let propertie = <div>this.props.selected;</div>;
+        if (this.props.selected !== null && this.props.selected !== undefined) {
+            propertie = (
+                <div>
+                    <span className="subTitle">{this.props.selected.state.name} Properties</span>
+                    <br />
+                    <br />
+                    {this.props.selected.state.properties.name}
+                </div>
+            );
+        }
 
-		let informations = (
-			<span className="info">
-				<span className="subTitle">{this.state.title}</span>
-				<br />
-				Left Click : Place blocks <br />
-				Right Click : Delete blocks
-				<br />
-				Middle Click : Pan the camera
-				<br />
-				Mouse Wheel : Zoom
-				<br />
-				<br />
-			</span>
-		);
+        let informations = (
+            <span className="info">
+                <span className="subTitle">{this.state.title}</span>
+                <br />
+                Left Click : Place blocks <br />
+                Right Click : Delete blocks
+                <br />
+                Middle Click : Pan the camera
+                <br />
+                Mouse Wheel : Zoom
+                <br />
+                <br />
+            </span>
+        );
 
-		return (
-			<div className="propertiesPanel" style={{ height: this.props.height }}>
-				<div className="header">{this.state.panelName}</div>
-				<div className="body">
-					{informations}
+        return (
+            <div className="propertiesPanel" style={{ height: this.props.height }}>
+                <div className="header">{this.state.panelName}</div>
+                <div className="body">
+                    {informations}
 
-					{propertie}
-				</div>
-			</div>
-		);
-	}
+                    {propertie}
+                </div>
+            </div>
+        );
+    }
 }
