1270c47a749b55fca17d1f1e42831a3cd3b03514
CodyAdam <cody.adam@laposte.net>
2020-04-07 10:49:08 +0200

    Changing the Tile And Tileset System

--- src/Editing/Tile.js
+++ src/Editing/Tile.js
@@ -1,28 +1,27 @@
 import { getCoordinateAt } from "./Helper";
 
 export default class Tile {
-    constructor(source, pos, relative) {
-        this.source = source;
+    constructor(image, tileSize, id, pos, data) {
+        this.image = image;
+        this.tileSize = tileSize;
+        this.id = id;
         this.pos = pos;
-        this.active = false;
-        this.relative = relative;
-
-        this.image = new Image();
-        this.image.src = this.source;
+        this.data = data;
     }
 
-    static isTileOnScreen(x, y, state) {
+    isTileOnScreen(container, grid) {
+        const { x, y } = this.pos;
         let topLeft = {
             x: 0,
             y: 0,
         };
         let bottomRight = {
-            x: state.container.width,
-            y: state.container.height,
+            x: container.width,
+            y: container.height,
         };
 
-        topLeft = getCoordinateAt(topLeft, state.grid);
-        bottomRight = getCoordinateAt(bottomRight, state.grid);
+        topLeft = getCoordinateAt(topLeft, grid);
+        bottomRight = getCoordinateAt(bottomRight, grid);
 
         topLeft.x += -1;
         topLeft.y += -1;
@@ -32,12 +31,47 @@ export default class Tile {
         if (x < topLeft.x || x > bottomRight.x || y < topLeft.y || y > bottomRight.y) return false;
         return true;
     }
+
+    update() {}
+
+    render({ container, grid }) {
+        const context = container.canvas.getContext("2d");
+        const offset = grid.offset;
+
+        if (!this.isTileOnScreen(container, grid)) return;
+
+        context.drawImage(
+            this.image,
+            offset.x + this.pos.x * grid.size,
+            offset.y + this.pos.y * grid.size,
+            grid.size,
+            grid.size,
+        );
+    }
+}
+
+export class Autotile extends Tile {
+    constructor(source, sourceJSON) {
+        super(source, { x: 0, y: 0 });
+        const { tileSize, autor, type, content } = JSON.parse(sourceJSON);
+
+        this.autotileID = 0;
+        this.tilesetID = -1; // not implemented
+
+        this.source = source;
+        this.pos = pos;
+        this.active = false;
+
+        this.image = new Image();
+        this.image.src = this.source;
+    }
+
     render(state) {
         const context = state.container.canvas.getContext("2d");
         const grid = state.grid;
         const offset = grid.offset;
 
-        if (!Tile.isTileOnScreen(this.pos.x, this.pos.y, state)) return;
+        if (!this.isTileOnScreen(state)) return;
 
         context.drawImage(
             this.image,
